f3211850163b3b6878dca3abe28fcc6954d034aa
alvinyanson <a.yanson@arcanys.com>
2024-08-27 12:24:24 +0800

    Add project files.

---
+++ API/Services/CoffeeShopService.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class CoffeeShopService : ICoffeeShopService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoffeeShopService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CoffeeShopModel>> List()
+        {
+            var coffeeShops = await (from shop in _context.CoffeeShops
+                                     select new CoffeeShopModel()
+                                     {
+                                         Id = shop.Id,
+                                         Name = shop.Name,
+                                         OpeningHours = shop.OpeningHours,
+                                         Address = shop.Address
+                                     }).ToListAsync();
+
+            return coffeeShops;
+        }
+    }
+}
